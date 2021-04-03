77cb494ad5e3117a3e8a698863be44a2d13fee0c
florianbgt <florianbgt@users.noreply.github.com>
2021-04-03 19:16:44 -0400

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,51 @@
+<template>
+  <h1>{{ msg }}</h1>
+
+  <h2>Vue 3 Bootstrap 5 simple modal component</h2>
+
+  <p>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Documentation</a> |
+    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+  </p>
+
+   <button @click="openModal()" type="button" class="btn btn-success mb-2">Open Modal</button>
+  <Modal v-model="modal" header="Modal">
+    <p>This modal have to be closed from the parent</p>
+    <p>This modal will be close from the parent in 5 seconds</p>
+  </Modal>
+
+  <br>
+
+  <button @click="closeableModal = true" type="button" class="btn btn-success mb-2">Open Closeable Modal</button>
+  <Modal v-model="closeableModal" closeable header="Closeable Modal">
+    <p>You can close this modal using the close button on the top right corner</p>
+  </Modal>  
+</template>
+
+<script setup>
+import { defineProps, reactive, ref } from 'vue'
+
+import Modal from './Modal.vue'
+
+defineProps({
+  msg: String
+})
+
+const closeableModal = ref(false);
+
+const modal = ref(false);
+
+const openModal = () => {
+  modal.value = true;
+  setTimeout(() => {
+    modal.value = false;
+  }, 5000);
+}
+
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,64 @@
+<template>
+    <teleport to="body">
+        <transition name="fade">
+            <div v-if="modelValue" class="pt-5 position-fixed top-0 start-0 h-100 w-100" style="background-color: rgba(0, 0, 0, 0.25)">
+                <div @click='$emit("update:modelValue", false)' class="backdrop"/>
+                <div class="modal1-dialog h-100 overflow-auto">
+                    <div class="card px-0" :class="(container == null) ? 'container' : `container-${container}`" :style="(maxwidth != null) ? `max-width: ${maxwidth}` : null">
+                        <div class="card-header h2">
+                            {{header}}
+                            <button v-if="closeable" @click='$emit("update:modelValue", false)' class="btn btn-text float-end"><strong>X</strong></button>
+                        </div>
+                        <div class="card-body">
+                            <slot></slot>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </transition>
+    </teleport>
+</template>
+
+<script setup>
+import { defineProps, ref } from 'vue';
+
+const props = defineProps({
+    modelValue: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    closeable: {
+        type: Boolean,
+        required: false,
+        default: false
+    },
+    header: {
+        type: String,
+        required: false,
+        default: null
+    },
+    container: {
+        type: String,
+        required: false,
+        default: null
+    },
+    maxwidth: {
+        type: String,
+        required: false,
+        default: null
+    }
+})
+
+const isVisible = ref(false);
+
+</script>
+
+<style scoped>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.5s
+}
+.fade-enter-from, .fade-leave-to{
+  opacity: 0
+}
+</style>
