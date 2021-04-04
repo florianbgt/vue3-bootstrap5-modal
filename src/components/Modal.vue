0d6931f284a98a56e66f2a10fe296a089cdbfd2a
florianbgt <florianbgt@users.noreply.github.com>
2021-04-04 18:58:18 -0400

    add backdrop click to close

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -2,8 +2,7 @@
     <teleport to="body">
         <transition name="fade">
             <div v-if="modelValue" class="pt-5 position-fixed top-0 start-0 h-100 w-100" style="background-color: rgba(0, 0, 0, 0.25)">
-                <div @click='$emit("update:modelValue", false)' class="backdrop"/>
-                <div class="modal1-dialog h-100 overflow-auto">
+                <div id="backdrop" @click="backdropClick" class="modal1-dialog h-100 overflow-auto">
                     <div class="card px-0" :class="(container == null) ? 'container' : `container-${container}`" :style="(maxwidth != null) ? `max-width: ${maxwidth}` : null">
                         <div class="card-header h2">
                             {{header}}
@@ -20,7 +19,13 @@
 </template>
 
 <script setup>
-import { defineProps, ref } from 'vue';
+import { defineProps, defineEmit, ref } from 'vue';
+
+const backdropClick = (event) => {
+    if (event.target.id == 'backdrop' && props.closeable == true) {
+        emit("update:modelValue", false);
+    }
+}
 
 const props = defineProps({
     modelValue: {
@@ -49,6 +54,8 @@ const props = defineProps({
         default: null
     }
 })
+
+const emit = defineEmit();
 
 const isVisible = ref(false);
 
